e6096d521cfbffec3fdf86363182c17a7fd79cf8
Massab Ali <amassab42@gamil.com>
2025-11-02 06:39:12 -0800

    github data

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-
+const Database = {
+  name : "massab",
+  class:"11",
+  databse :"pass"
+}
 app.get('/', (req, res) => {
   res.send('Hel ')
 })
+
+app.get('/url',(req,res)=>{
+  res.json(Database)
+})  
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
